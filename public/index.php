0cb658536c962c596dd54242aef8583482ae77c7
Notpapu78 <a.chavezmozo@liceorbl.cl>
2026-08-30 23:29:47 -0400

    Upload

---
+++ public/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <title>KYbrary</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="pageTitle">
+                <h1>KYbrary</h1>
+            </div>
+            <div class="menuItem">
+                <img src="https://imgs.search.brave.com/XCVIkZkF-xCwczplFgnjBZnX1wfkN7MD-6227b_sH20/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/aWNvbnNjb3V0LmNv/bS9pY29uL3ByZW1p/dW0vcG5nLTI1Ni10/aHVtYi9tZW51LWlj/b24tc3ZnLWRvd25s/b2FkLXBuZy0yMDQw/NDUucG5nP2Y9d2Vi/cCZ3PTEyOA" alt="" width="60px" height="60px">
+            </div>
+        </nav>
+    </header>
+    
+    <main>
+        <section id="introduction">
+            <article>
+                <h1>Bienvenido a KYbrary</h1>
+            </article>
+        </section>
+    </main>
+
+    <footer>
+        <p>Copyright &copy;2026 KYbrary Todos los derechos reservados</p>
+        <p>Contacto: a.alfaro@liceorbl.cl</p>
+    </footer>
+</body>
+</html>
